386db0ab7ee945c14967bd51121f495952974352
D1tt0 <dittozhang09@gmail.com>
2023-06-23 00:56:10 +0800

    Version 1.0.0

--- vultr_api.py
+++ vultr_api.py
@@ -1,38 +1,35 @@
 import json
 import requests
 
+
 class vultr_api:
     """
     Created for a personal project and only implemented the methods I needed.
     """
     def __init__(self, api_token):
         self.api_token = api_token
-        if not self._check_token():
-            raise Exception("[Error] token invalid")
-
-    def _check_token(self) -> bool:
+        # check token
         req = self._call_api("GET", "account")
-        if req.status_code == 200:
-            return True
-        else:
-            return False
+        if req.status_code != 200:
+            print(f"[Error] Token invalid.\nMsg:{req.text}")
+            raise
 
     def _call_api(self, method, endpoint, data=None):
         method = method.upper()
         API_ENTRANCE_URL = "https://api.vultr.com/v2/"
-        headers =  {
-            "Authorization" : "Bearer " + self.api_token
+        headers = {
+            "Authorization": "Bearer " + self.api_token
         }
         if method == "GET":
             return requests.get(API_ENTRANCE_URL + endpoint, headers=headers)
         elif method == "POST":
-            return requests.post(API_ENTRANCE_URL + endpoint, 
-                                 headers=headers, 
+            return requests.post(API_ENTRANCE_URL + endpoint,
+                                 headers=headers,
                                  data=json.dumps(data))
         elif method == "DELETE":
-            return requests.delete(API_ENTRANCE_URL + endpoint, 
-                                 headers=headers, 
-                                 data=json.dumps(data))
+            return requests.delete(API_ENTRANCE_URL + endpoint,
+                                   headers=headers,
+                                   data=json.dumps(data))
         else:
             raise Exception("[Error] This method is not supported currently")
 
@@ -45,18 +42,18 @@ class vultr_api:
         endpoint = "firewalls"
         req = self._call_api("GET", endpoint)
         return req.text
-    
-    def list_fw_rules(self, gw_group_id:str) -> str:
+
+    def list_fw_rules(self, gw_group_id: str) -> str:
         endpoint = f"firewalls/{gw_group_id}/rules"
         req = self._call_api("GET", endpoint)
         return req.text
 
-    def create_fw_rules(self, gw_group_id:str, rule_info:dict) -> str:
+    def create_fw_rules(self, gw_group_id: str, rule_info: dict) -> str:
         endpoint = f"firewalls/{gw_group_id}/rules"
         req = self._call_api("POST", endpoint, rule_info)
         return req.text
-    
-    def del_fw_rules(self, gw_group_id:str, rule_id:int) -> str:
+
+    def del_fw_rules(self, gw_group_id: str, rule_id: int) -> str:
         endpoint = f"firewalls/{gw_group_id}/rules/{rule_id}"
         req = self._call_api("DELETE", endpoint, rule_id)
         return req.text
